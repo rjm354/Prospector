675d5da2884919a4e93147da7a07cae46c28e388
rjm354 <rjm354@rutgers.edu>
2016-12-18 01:40:21 -0500

    Finished Prospector (Bartok)
    The Bartok prototype is unfinished. The Bartok XML is read which sorts
    part of the deck.

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -2,43 +2,101 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
-	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;     // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
 
-	
-	// property
-	public bool faceUP {
-		get {
-			return (!back.activeSelf);
-		}		
-		set {
-			back.SetActive(!value);
-		}
-	}	
+    public GameObject back;     // back of card;
+    public CardDefinition def;  // from DeckXML.xml	
+
+    public SpriteRenderer[] spriteRenderers;
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
+    }
+
+    void Start()
+    {
+        SetSortOrder(0);
+    }
+
+    // property
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
 } // class Card
 
 [System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
+public class Decorator
+{
+    public string type;         // For card pips, tyhpe = "pip"
+    public Vector3 loc;         // location of sprite on the card
+    public bool flip = false;   //whether to flip vertically
+    public float scale = 1.0f;
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used					
+public class CardDefinition
+{
+    public string face; //sprite to use for face cart
+    public int rank;    // value from 1-13 (Ace-King)
+    public List<Decorator> pips = new List<Decorator>();  // Pips Used					
 } // Class CardDefinition
